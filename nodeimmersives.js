91b80f931686fad01bd25e6a146de61b5b1f2e77
Gregory.Johnson <3531op@gmail.com>
2020-09-28 15:27:47 -0400

    Fixed punctuation mistakes

--- nodeimmersives.js
+++ nodeimmersives.js
@@ -1,6 +1,6 @@
 let firstName = process.argv[2];
 let lastName = process.argv[3];
-console.log ("Hi " + (firstName + " " + lastName));
-console.log ("I am capitalizing the name " + firstName.toUpperCase())
-console.log ("I am getting initials " + firstName[0] + "." + lastName[0] + ".")
-console.log ("I am giving you your email " + firstName.toLowerCase() + "." + lastName.toLowerCase() + "@codeimmersives.com")
+console.log ("Hi! " + (firstName + " " + lastName + "."));
+console.log ("I am capitalizing your name. " + firstName.toUpperCase())
+console.log ("I am getting initials. " + firstName[0] + "." + lastName[0] + ".")
+console.log ("I am giving you your email. " + firstName.toLowerCase() + "." + lastName.toLowerCase() + "@codeimmersives.com")
